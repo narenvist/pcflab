158aec48f794f2ece346e9b3532da1a87fdc8a1b
kishorepkr <45389648+kishorepkr@users.noreply.github.com>
2019-03-14 12:46:26 +0530

    Add files via upload

---
+++ start-apps/tasks/cf-deployment.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -x
+
+target="cf api $API_ENDPOINT --skip-ssl-validation"
+#echo $target
+eval $target
+
+echo "Login....."
+login="cf auth $USERNAME $PASSWORD"
+#echo $login
+eval $login
+
+echo "Target Org and Space"
+org_space="cf target -o $ORG -s $SPACE"
+eval $org_space
+
+applications=`cat misc-repo/start-apps/app-list.json | jq -r '.[].application'`
+
+
+echo "$CF_SUB_COMMAND the app"
+for app in $applications
+do
+  eval "cf $CF_SUB_COMMAND $app"
+done
