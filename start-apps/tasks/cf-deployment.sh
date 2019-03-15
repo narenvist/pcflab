ee99a8efb5b2777ed73ccd5e37231e7b4544794f
muntha123 <47027255+muntha123@users.noreply.github.com>
2019-03-15 14:19:43 +0000

    Update cf-deployment.sh

--- start-apps/tasks/cf-deployment.sh
+++ start-apps/tasks/cf-deployment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+COUNT=5
 
 target="cf api $API_ENDPOINT --skip-ssl-validation"
 #echo $target
@@ -16,9 +17,26 @@ eval $org_space
 
 applications=`cat pcflab/start-apps/app-list.json | jq -r '.[].application'`
 
-
 echo "$CF_SUB_COMMAND the app"
+
+restart_fun(){
+        echo "cf $CF_SUB_COMMAND $app"
+        }
+
 for app in $applications
-do
-  eval "cf $CF_SUB_COMMAND $app"
-done
+        do
+                eval "cf $CF_SUB_COMMAND $app"
+                status=$(cf app $app  | grep 'requested state:'| awk '{print $3}')
+
+        if [ "$STATUS" = "started" ]
+        then
+                echo "$app is started"
+                break
+        else
+                echo "$STATUS"
+                echo "App not started, trying to restatrt. Attmept $i" | mailx -s "Restart APP $app" ravanaiah.m@cognizant.com
+                for ((i=0;i<$COUNT;i++))
+                {
+                restart_fun
+                }
+        fi
