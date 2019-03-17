543df29d062a566f4dec21e2bc763659ddc96c4b
muntha123 <47027255+muntha123@users.noreply.github.com>
2019-03-17 11:04:10 +0530

    Update cf-deployment.sh

--- start-apps/tasks/cf-deployment.sh
+++ start-apps/tasks/cf-deployment.sh
@@ -19,24 +19,40 @@ applications=`cat pcflab/start-apps/app-list.json | jq -r '.[].application'`
 
 echo "$CF_SUB_COMMAND the app"
 
-restart_fun(){
-        echo "cf $CF_SUB_COMMAND $app"
-        }
-
 for app in $applications
         do
-                eval "cf $CF_SUB_COMMAND $app"
-                status=$(cf app $app  | grep 'requested state:'| awk '{print $3}')
+                for j in 1 2 3 4 5
+                do 
+                         #cf start $i
+                         STATUS= cf app $i | grep -e " requested state:"|awk '{print  $3}'
+                         if [[ $STATUS =="stopped" ]]
+                         then
+                                echo -e "App is still down\n"
+                                if [[ $j == 1 ]]
+                                then
+                                        echo "Triggering a mail to user :ravanaiah (ravanaiahweblogic@gail.com)"
+                                        mail -s "automation to bring up app failed for $1 app" ravanaiahweblogic@gmail.com <<< "automation to bring up this $i app got failed"
+                                fi
+                                cf start $i
+                                continue
+                          else
+                                echo "App is up and running"
+                                break
+                          fi
+                   done
+          done         
+                #eval "cf $CF_SUB_COMMAND $app"
+                #status=$(cf app $app  | grep 'requested state:'| awk '{print $3}')
 
-        if [ "$STATUS" = "started" ]
-        then
-                echo "$app is started"
-                break
-        else
-                echo "$STATUS"
-                echo "App not started, trying to restatrt. Attmept $i" | mailx -s "Restart APP $app" ravanaiah.m@cognizant.com
-                for ((i=0;i<$COUNT;i++))
-                {
-                restart_fun
-                }
-        fi
+        #if [ "$STATUS" = "started" ]
+        #then
+         #       echo "$app is started"
+          #      break
+        #else
+         #       echo "$STATUS"
+          #      echo "App not started, trying to restatrt. Attmept $i" | mailx -s "Restart APP $app" ravanaiah.m@cognizant.com
+           #     for ((i=0;i<$COUNT;i++))
+            #    {
+             #   restart_fun
+              #  }
+        #fi
