b2d0e4d58b5b1f7fcfe913e6ddde8cad98c16ec1
narenvist <42965327+narenvist@users.noreply.github.com>
2019-03-19 08:20:58 +0000

    Update cf-deployment.sh

--- start-apps/tasks/cf-deployment.sh
+++ start-apps/tasks/cf-deployment.sh
@@ -23,30 +23,30 @@ echo "$CF_SUB_COMMAND the app"
 
 for app in $applications
     do
-                while [[ $iteration -le $COUNT ]]
-                do 
-                    cf $CF_SUB_COMMAND $app
-                    STATUS=`cf app $app | tail -n 1 | awk '{print $2}'`
+        while [[ $iteration <= $COUNT ]]
+        do 
+			cf $CF_SUB_COMMAND $app
+			STATUS=`cf app $app | tail -n 1 | awk '{print $2}'`
+			sleep 5s
+			if [[ $STATUS != "running" ]]
+			then
+				echo -e "App is down\n"
+				if [[ $iteration == $COUNT ]]
+				then
+					echo "Triggering a mail to user :kishore (kishore.ponnuru.contractor@pepsico.com)"
+					#mail -s "CRITICAL: Automation to bring up app failed for $app app" kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
+					break
+				else
+					#mail -s "Warning Attempt:$iteration to start $app app failed"  kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
+					echo "For Iteration:$iteration restarting app $app"
+					cf $CF_SUB_COMMAND $app
 					sleep 5s
-                    if [[ $STATUS == "stopped" ]]
-                    then
-                        echo -e "App is down\n"			
-                        if [[ $iteration == $COUNT ]]
-                        then
-							echo "Triggering a mail to user :kishore (kishore.ponnuru.contractor@pepsico.com)"
-							#mail -s "CRITICAL: Automation to bring up app failed for $app app" kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
-							break
-						else
-							#mail -s "Warning Attempt:$iteration to start $app app failed"  kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
-							echo "For Iteration:$iteration restarting app $app"
-							cf $CF_SUB_COMMAND $app
-							sleep 5s
-							continue
-						fi
-                    else
-						echo "$app is up and running"
-						break
-                    fi
-				iteration=`expr $iteration + 1`
-                done
+					continue
+				fi
+			else
+				echo "$app is up and running"
+				break
+			fi
+			iteration=`expr $iteration + 1`
+		done
 	done
