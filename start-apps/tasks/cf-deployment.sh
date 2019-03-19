0e2226acc440f6d6a4c92e6a242d020e9a717829
narenvist <42965327+narenvist@users.noreply.github.com>
2019-03-19 08:03:31 +0000

    Update cf-deployment.sh

--- start-apps/tasks/cf-deployment.sh
+++ start-apps/tasks/cf-deployment.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -x
 COUNT=3
 iteration=1
@@ -21,32 +22,31 @@ applications=`cat pcflab/start-apps/app-list.json | jq -r '.[].application'`
 echo "$CF_SUB_COMMAND the app"
 
 for app in $applications
-        do
+    do
                 while [[ $iteration -le $COUNT ]]
                 do 
-                         cf $CF_SUB_COMMAND $app
-                         STATUS=`cf app $app | tail -n 1 | awk '{print $2}'`
-			 sleep 5s
-                         if [[ $STATUS != "running" ]]
-                         then
-                                echo -e "App is down\n"			
-                                if [[ $iteration == $COUNT ]]
-                                then
-                                        echo "Triggering a mail to user :kishore (kishore.ponnuru.contractor@pepsico.com)"
-                                        #mail -s "CRITICAL: Automation to bring up app failed for $app app" kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
-                                        break
-                                else
-                                #mail -s "Warning Attempt:$iteration to start $app app failed"  kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
-				echo "For Iteration:$iteration restarting app $app"
-				cf $CF_SUB_COMMAND $app
-                                sleep 5s
-				continue
-				fi
-                          else
-                                echo "$app is up and running"
-                                break
-                          fi
-			   iteration=$iteration+1
-                   done
-          done         
-                
+                    cf $CF_SUB_COMMAND $app
+                    STATUS=`cf app $app | tail -n 1 | awk '{print $2}'`
+					sleep 5s
+                    if [[ $STATUS == "stopped" ]]
+                    then
+                        echo -e "App is down\n"			
+                        if [[ $iteration == $COUNT ]]
+                        then
+							echo "Triggering a mail to user :kishore (kishore.ponnuru.contractor@pepsico.com)"
+							#mail -s "CRITICAL: Automation to bring up app failed for $app app" kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
+							break
+						else
+							#mail -s "Warning Attempt:$iteration to start $app app failed"  kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
+							echo "For Iteration:$iteration restarting app $app"
+							cf $CF_SUB_COMMAND $app
+							sleep 5s
+							continue
+						fi
+                    else
+						echo "$app is up and running"
+						break
+                    fi
+					iteration=$iteration+1
+                done
+	done
