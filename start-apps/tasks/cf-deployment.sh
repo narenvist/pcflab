46d1a311025eb58f6b4823c6af4ea693902d09d8
narenvist <42965327+narenvist@users.noreply.github.com>
2019-04-12 09:29:59 +0000

    Update cf-deployment.sh

--- start-apps/tasks/cf-deployment.sh
+++ start-apps/tasks/cf-deployment.sh
@@ -21,36 +21,34 @@ applications=`cat pcflab/start-apps/app-list.json | jq -r '.[].application'`
 echo "$CF_SUB_COMMAND the app"
 
 for app in $applications
-    do
-        cf $CF_SUB_COMMAND $app
-	sleep 5s
-	i=1
+do
+	cf $CF_SUB_COMMAND $app
+done
+
+for app in $applications
+do
+	STATUS=`cf app $app | tail -n 1 | awk '{print $2}'`
+    	i=1
 	while [ $i -le $COUNT ] 
-	do
-		STATUS=`cf app $app | tail -n 1 | awk '{print $2}'`
+	do			
 		if [ $STATUS != "running" ]
 		then
 			echo -e "App is down\n"
 			if [ $i == $COUNT ]
 			then
-				echo "Triggering a mail to user :kishore (kishore.ponnuru.contractor@pepsico.com)"
-				#mail -s "CRITICAL: Automation to bring up app failed for $app app" kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
+				echo "Triggering a mail to :EIAP Alerts (ITEIAPALERTS@pepsico.com)"
+				mail -s "CRITICAL: Automation to bring up app failed for $app app" ITEIAPALERTS@pepsico.com  <<< "automation to bring up this $app app got failed"
 				break
 			else
-				#mail -s "Warning Attempt:$iteration to start $app app failed"  kishore.ponnuru.contractor@pepsico.com <<< "automation to bring up this $app app got failed"
+				mail -s "Warning Attempt:$iteration to start $app app failed"  ITEIAPALERTS@pepsico.com  <<< "automation to bring up this $app app got failed"
 				echo "For Iteration:$i restarting app $app"
 				cf $CF_SUB_COMMAND $app
-				sleep 5s
 				#continue
 			fi
 		else
 			echo "$app is up and running"
 			break
 		fi
-		echo "before increment: "$i
-		#$i++
-		
 		i=$(( $i+1 ))
-		echo "after increment: "$i
 	done
 done
